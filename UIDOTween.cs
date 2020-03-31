e6d677ce28b593710ef06cc1c9727212d879f55b
GH-ZJ <wustzhangjie@gmail.com>
2020-04-01 01:39:33 +0800

    refine UIDOTween

--- UIDOTween.cs
+++ UIDOTween.cs
@@ -9,16 +9,15 @@ namespace GameUtil
     {
         public bool PlayOnEnable = true;
         public SingleTween[] StartSingleTweens = new SingleTween[0];
-        public SingleTween[] EndSingleTweens = new SingleTween[0];
+        public SingleTween[] CloseSingleTweens = new SingleTween[0];
         
-        public bool IsExecuteStartAction = true;
-        [Tooltip("打开界面动画结束时调用")] public UnityEvent StartEvent;
+        public bool IsExecuteStartAction = true;//是否执行DoStartTween(action) 里的action
+        public bool IsExecuteCloseAction = true;//是否执行DoCloseTween(action) 里的action
 
-        public bool IsExecuteCloseAction = true;
-        [Tooltip("关闭界面动画结束时调用")] public UnityEvent CloseEvent;
-
-        [Tooltip("打开界面时调用")] public UnityEvent OnStart;
-        [Tooltip("关闭界面时调用")] public UnityEvent OnClose;
+        [Tooltip("打开界面时调用")] public UnityEvent OnStartBeforeAnim;
+        [Tooltip("关闭界面时调用")] public UnityEvent OnCloseBeforeAnim;
+        [Tooltip("打开界面动画结束时调用")] public UnityEvent OnStartAfterAnim;
+        [Tooltip("关闭界面动画结束时调用")] public UnityEvent OnCloseAfterAnim;
         
         private Sequence mSequence;
         
@@ -27,7 +26,7 @@ namespace GameUtil
             foreach (var tween in StartSingleTweens)
                 tween.Bind(gameObject);
 
-            foreach (var tween in EndSingleTweens)
+            foreach (var tween in CloseSingleTweens)
                 tween.Bind(gameObject);
         }
         
@@ -40,12 +39,12 @@ namespace GameUtil
         public void DoStartTween(Action action)
         {
             RecoverStatus();
-            DoTweenInternal(StartSingleTweens, OnStart, IsExecuteStartAction, StartEvent, action);
+            DoTweenInternal(StartSingleTweens, OnStartBeforeAnim, IsExecuteStartAction, OnStartAfterAnim, action);
         }
 
         public void DoCloseTween(Action action)
         {
-            DoTweenInternal(EndSingleTweens, OnClose, IsExecuteCloseAction, CloseEvent, action);
+            DoTweenInternal(CloseSingleTweens, OnCloseBeforeAnim, IsExecuteCloseAction, OnCloseAfterAnim, action);
         }
         
         [ContextMenu("DoStartTween")]
@@ -65,6 +64,7 @@ namespace GameUtil
             float lastTweenInsertTime = 0;
             mSequence?.Kill();
             mSequence = null;
+            beforeEvent?.Invoke();
             if (tweens.Length > 0)
             {
                 mSequence = DOTween.Sequence();
@@ -101,24 +101,22 @@ namespace GameUtil
                 mSequence.AppendCallback(() =>
                 {
                     mSequence = null;
-                    if (!isExecute) return;
-                    mSequence = null;
-                    action?.Invoke();
                     afterEvent?.Invoke();
+                    if (isExecute)
+                        action?.Invoke();
                 });
             }
-            else if (isExecute)
+            else
             {
-                action?.Invoke();
                 afterEvent?.Invoke();
+                if (isExecute)
+                    action?.Invoke();
             }
-
-            beforeEvent?.Invoke();
         }
 
         private void RecoverStatus()
         {
-            foreach (var tween in EndSingleTweens)
+            foreach (var tween in CloseSingleTweens)
                 tween.RecoverStatus();
             
             foreach (var tween in StartSingleTweens)
