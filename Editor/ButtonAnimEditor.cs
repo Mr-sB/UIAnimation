2362a962514453e76b4de5ee1cf47bad45b25046
GH-ZJ <wustzhangjie@gmail.com>
2020-06-19 18:34:48 +0800

    Modify ButtonAnimEditor

--- Editor/ButtonAnimEditor.cs
+++ Editor/ButtonAnimEditor.cs
@@ -26,25 +26,26 @@ namespace GameUtil.Editor
             SerializedProperty iterator = serializedObject.GetIterator();
             for (bool enterChildren = true; iterator.NextVisible(enterChildren); enterChildren = false)
             {
-                using (new EditorGUI.DisabledScope("m_Script" == iterator.propertyPath))
+                switch (iterator.propertyPath)
                 {
-                    switch (iterator.propertyPath)
+                    case "m_Script":
+                        using (new EditorGUI.DisabledScope(true))
+                            EditorGUILayout.PropertyField(iterator, true);
+                        break;
+                    case nameof(ButtonAnim.NeedSecondUpSetting):
+                        EditorGUILayout.PropertyField(iterator, true);
+                        mShowSecondUpSetting.target = iterator.boolValue;
+                        break;
+                    case nameof(ButtonAnim.SecondUpSetting):
                     {
-                        case nameof(ButtonAnim.NeedSecondUpSetting):
+                        if (EditorGUILayout.BeginFadeGroup(mShowSecondUpSetting.faded))
                             EditorGUILayout.PropertyField(iterator, true);
-                            mShowSecondUpSetting.target = iterator.boolValue;
-                            break;
-                        case nameof(ButtonAnim.SecondUpSetting):
-                        {
-                            if (EditorGUILayout.BeginFadeGroup(mShowSecondUpSetting.faded))
-                                EditorGUILayout.PropertyField(iterator, true);
-                            EditorGUILayout.EndFadeGroup();
-                            break;
-                        }
-                        default:
-                            EditorGUILayout.PropertyField(iterator, true);
-                            break;
+                        EditorGUILayout.EndFadeGroup();
+                        break;
                     }
+                    default:
+                        EditorGUILayout.PropertyField(iterator, true);
+                        break;
                 }
             }
             serializedObject.ApplyModifiedProperties();
