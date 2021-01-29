7504083435b055c311bf7a932b6e40a0b211628b
GH-ZJ <wustzhangjie@gmail.com>
2021-01-29 15:42:18 +0800

    Add UIDOTweenDrawer editor class, draw StartSingleTweens and StartSingleTweens as ReorderableList.

---
+++ Editor/UIDOTweenDrawer.cs
@@ -0,0 +1,79 @@
+using System;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace GameUtil.Editor
+{
+    [CustomEditor(typeof(UIDOTween))]
+    public class UIDOTweenDrawer : UnityEditor.Editor
+    {
+        private const float ELEMENT_OFFSET_X = 5;
+        private const float ELEMENT_OFFSET_Y = 2;
+        private ReorderableList mStartSingleTweenReorderableList;
+        private ReorderableList mCloseSingleTweenReorderableList;
+
+        private void OnEnable()
+        {
+            //StartSingleTweenReorderableList
+            mStartSingleTweenReorderableList = new ReorderableList(serializedObject, serializedObject.FindProperty(nameof(UIDOTween.StartSingleTweens)));
+            mStartSingleTweenReorderableList.drawHeaderCallback = rect =>
+            {
+                EditorGUI.LabelField(rect, mStartSingleTweenReorderableList.serializedProperty.displayName);
+            };
+            mStartSingleTweenReorderableList.drawElementCallback = (rect, index, active, focused) =>
+            {
+                rect.x += ELEMENT_OFFSET_X;
+                rect.width -= ELEMENT_OFFSET_X;
+                rect.y += ELEMENT_OFFSET_Y;
+                EditorGUI.PropertyField(rect, mStartSingleTweenReorderableList.serializedProperty.GetArrayElementAtIndex(index));
+            };
+            mStartSingleTweenReorderableList.elementHeightCallback = index =>
+                EditorGUI.GetPropertyHeight(mStartSingleTweenReorderableList.serializedProperty.GetArrayElementAtIndex(index)) + ELEMENT_OFFSET_Y;
+            
+            //CloseSingleTweenReorderableList
+            mCloseSingleTweenReorderableList = new ReorderableList(serializedObject, serializedObject.FindProperty(nameof(UIDOTween.CloseSingleTweens)));
+            mCloseSingleTweenReorderableList.drawHeaderCallback = rect =>
+            {
+                EditorGUI.LabelField(rect, mCloseSingleTweenReorderableList.serializedProperty.displayName);
+            };
+            mCloseSingleTweenReorderableList.drawElementCallback = (rect, index, active, focused) =>
+            {
+                rect.x += ELEMENT_OFFSET_X;
+                rect.width -= ELEMENT_OFFSET_X;
+                rect.y += ELEMENT_OFFSET_Y;
+                EditorGUI.PropertyField(rect, mCloseSingleTweenReorderableList.serializedProperty.GetArrayElementAtIndex(index));
+            };
+            mCloseSingleTweenReorderableList.elementHeightCallback = index =>
+                EditorGUI.GetPropertyHeight(mCloseSingleTweenReorderableList.serializedProperty.GetArrayElementAtIndex(index)) + ELEMENT_OFFSET_Y;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            serializedObject.UpdateIfRequiredOrScript();
+            SerializedProperty iterator = serializedObject.GetIterator();
+            for (bool enterChildren = true; iterator.NextVisible(enterChildren); enterChildren = false)
+            {
+                switch (iterator.propertyPath)
+                {
+                    case "m_Script":
+                        using (new EditorGUI.DisabledScope(true))
+                            EditorGUILayout.PropertyField(iterator, true);
+                        break;
+                    case nameof(UIDOTween.StartSingleTweens):
+                        mStartSingleTweenReorderableList.DoLayoutList();
+                        break;
+                    case nameof(UIDOTween.CloseSingleTweens):
+                        mCloseSingleTweenReorderableList.DoLayoutList();
+                        break;
+                    default:
+                        EditorGUILayout.PropertyField(iterator, true);
+                        break;
+                }
+            }
+            serializedObject.ApplyModifiedProperties();
+            EditorGUI.EndChangeCheck();
+        }
+    }
+}
